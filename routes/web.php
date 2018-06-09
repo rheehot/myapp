fb19a6a01a08a38d89fd49eaf32dd4772da1f3c5
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-06-09 20:49:26 +0900

    라우팅

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,32 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+////    return view('welcome');
+//    return "<h1>Hello Foo</h1>";
+//});
+
+//Route::get('/{foo}', function ($foo) {
+//    return $foo;
+//});
+
+//Route::pattern('foo', '[0-9a-zA-Z]{3}');
+//
+//Route::get('/{foo?}', function ($foo="bar") {
+//    return $foo;
+//});
+
+//Route::get('/{foo?}', function ($foo="bar") {
+//    return $foo;
+//})->where('foo','[0-9a-zA-Z]{3}');
+
+Route::get('/', [
+    'as' => 'home',
+    function() {
+        return '제 이름은 "home" 입니다';
+    }
+]);
+
+Route::get('/home', function () {
+    return redirect(route('home'));
 });
