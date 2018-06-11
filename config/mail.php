54ec6ab8e4ff18d737b316c3e691ee6384d9b65e
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-06-11 13:42:55 +0900

    사용자 인증

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,10 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+//        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+//        'name' => env('MAIL_FROM_NAME', 'Example'),
+            'address' => env('john@example.com'),
+            'name' => env('Admin'),
     ],
 
     /*
